7e636b8ef4ec7e2f414105ee15c1eef913285fee
Shehbaz Ahmad <shehbazbashir@gmail.com>
2020-10-15 14:15:11 +0100

    Move checks into validate function

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
 require_relative 'errors_exceptions/errors.rb'
-require 'byebug'
+
 class LogParser
   attr_reader :log_path
 
   def initialize(log_path)
+    @log_path = log_path
+    validate
+  end
+
+  private
+
+  def validate
     raise LogPathRequired if log_path.nil? || log_path.empty?
     raise LogPathInvalid unless File.exist?(log_path)
   end
