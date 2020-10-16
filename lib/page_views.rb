18a53283780e1999ea27163ad0a7e1cd7e28eced
Shehbaz Ahmad <shehbazbashir@gmail.com>
2020-10-16 12:05:53 +0100

    unique visits sorted

--- lib/page_views.rb
+++ lib/page_views.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'byebug'
 
 class PageViews
   def initialize(page_views)
@@ -6,7 +7,11 @@ class PageViews
   end
 
   def most_views
-    formatted_views
+    formatted_most_views
+  end
+
+  def unique_views
+    formatted_unique_views
   end
 
   private
@@ -15,7 +20,15 @@ class PageViews
     @page_views.sort_by { |_page, views| -views.count }
   end
 
-  def formatted_views
+  def unique_views_sorted
+    @page_views.sort_by { |_page, views| -views.uniq.count }
+  end
+
+  def formatted_most_views
     most_views_sorted.map { |page, views| "#{page} #{views.count} visits" }
+  end
+
+  def formatted_unique_views
+    unique_views_sorted.map { |page, views| "#{page} #{views.uniq.count} unique views" }
   end
 end
--- parser.rb
+++ parser.rb
@@ -6,4 +6,5 @@ require_relative 'lib/log_parser.rb'
 require_relative 'lib/page_views.rb'
 
 page_views = LogParser.new('webserver.log').parse
-puts PageViews.new(page_views).most_views
+# puts PageViews.new(page_views).most_views
+puts PageViews.new(page_views).unique_views
